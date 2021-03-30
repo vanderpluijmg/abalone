605da6a97787e992ebb7be75659dac2fe5ffa27a
jaraczi <38017049+jaraczi@users.noreply.github.com>
2021-03-30 19:47:01 +0200

    début board to string

--- Abalone/Model/board.cpp
+++ Abalone/Model/board.cpp
@@ -1,6 +1,6 @@
 #include "board.h"
 #include "hexagon.hpp"
-/**
+
 Board::Board()
 {
 }
@@ -14,13 +14,13 @@ void Board::getInitBoard()
                 (i == 1 && (j == 3 || j == 4 || j == 5 || j == 6 || j == 7 || j == 8)) ||
                 (i == 2 && (j == 4 || j == 5 || j == 6)))
             {
-                _gameBoard[i][j] = Hexagon(WHITE, i, j);
+                _gameBoard[j][i] = Hexagon(WHITE, j, i);
             }
             else if ((i == 6 && (j == 2 || j == 3 || j == 4)) ||
                      (i == 7 && (j == 0 || j == 1 || j == 2 || j == 3 || j == 4 || j == 5)) ||
                      (i == 8 && (j == 0 || j == 1 || j == 2 || j == 3 || j == 4)))
             {
-                _gameBoard[i][j] = Hexagon(BLACK, i, j);
+                _gameBoard[j][i] = Hexagon(BLACK, j, i);
             }
             else if (((i == 0 && (j == 0 || j == 1 || j == 2 || j == 3)) ||
                       (i == 1 && (j == 0 || j == 1 || j == 2)) ||
@@ -31,11 +31,11 @@ void Board::getInitBoard()
                       (i == 7 && (j == 6 || j == 7 || j == 8)) ||
                       (i == 8 && (j == 5 || j == 6 || j == 7 || j == 8))))
             {
-                _gameBoard[i][j] = Hexagon(OUTOFBOUND, i, j);
+                _gameBoard[j][j] = Hexagon(OUTOFBOUND, j, i);
             }
             else
             {
-                _gameBoard[i][j] = Hexagon(EMPTY, i, j);
+                _gameBoard[j][i] = Hexagon(EMPTY, j, i);
             }
         }
     }
@@ -45,4 +45,21 @@ Hexagon Board::getHexagon(int x, int y)
 {
     return _gameBoard[x][y];
 }
-*/
+bool Board::checkNeigbourSameColor(Position position, Direction direction)
+{
+    Color origin = _gameBoard[position.getCol()][position.getRow()].getMarbleColor();
+    Color comparedTo = _gameBoard[position.getCol() + direction.getDeltaX()][position.getRow() + direction.getDeltaY()].getMarbleColor();
+    return origin == comparedTo;
+}
+
+std::ostream &operator<<(std::ostream &stream,Board board){
+for (int i = 0; i < 9; i++)
+{
+stream << lineToString(stream,board,i);
+}
+
+}
+
+std::ostream lineToString(std::ostream &stream,Board board,int line){
+
+}
--- Abalone/Model/board.h
+++ Abalone/Model/board.h
@@ -7,7 +7,8 @@
 class Board
 {
 private:
-    Hexagon _gameBoard[9][9];
+    Hexagon _gameBoard[9][9] = {};
+
 public:
     /**
      * @brief Default board constructor.
@@ -24,8 +25,31 @@ public:
      * @return Returns a vector of vector for 2 players.
      */
     Hexagon getHexagon(int x, int y);
+
+    /**
+    * @brief Verifies if a neighbour of a specific position in a given Direction has the same Color.
+    * 
+    * @param position a given Position.
+    * @param direction a given direction to follow.
+    * @return true if they are the same Color.
+    * @return false if they are not of the same Color.
+    */
+    bool checkNeigbourSameColor(Position position, Direction direction);
+
+    /**
+    * @brief Returns the Board as a string that can then be used to Display it in the terminal.
+    * 
+    * @return std::string the board as a String.
+    */
+    friend std::ostream &operator<<(std::ostream &stream,Board board);
+
+    /**
+     * @brief Sends back the requested line of the _gameBoard as a string.
+     * 
+     * @param line indicates which line it is.
+     * @return std::string the outcome.
+     */
+    friend std::ostream lineToString(std::ostream &stream,Board board,int line);
 };
-
-
 
 #endif // BOARD_H
