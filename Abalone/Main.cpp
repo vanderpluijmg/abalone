a3c9706d5e399bdbf5cbe0e8d02001b9cfffd9a8
54786 <54786@etu.he2b.be>
2021-04-01 14:52:21 +0200

    fixes

--- Abalone/Main.cpp
+++ Abalone/Main.cpp
@@ -11,7 +11,7 @@ using namespace::std;
 
 int main (void) {
     MoveUtils a;
-    a = AbaPro::getCommand("C3C5D3"); //Direction verification.
+    a = AbaPro::getCommand("I8H7"); //Direction verification.
     game game;
     cout<<game.validateLineAndSameColor(a);
 }
